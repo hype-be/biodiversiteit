a2a0782dd79f3e84b6cad34ee01b7a3d80368a33
bartnelissen <bart@hyp3.be>
2022-07-07 16:02:55 +0200

    Paragraph slider quotes.

--- themes/custom/biodiv/js/custom.js
+++ themes/custom/biodiv/js/custom.js
@@ -33,6 +33,15 @@
       prevArrow: $('.gallery-prev'),
       nextArrow: $('.gallery-next'),
     });
+
+    $('.slider-quotes').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      accessibility: true,
+      arrows: false,
+      dots: true
+    });
   });
 
   function isInViewport(node) {
