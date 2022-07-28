c1acc5b463a77aed42be4ecc5c30b1c4b49e1ee6
bartnelissen <bart@hyp3.be>
2022-07-28 16:38:30 +0200

    Settings page.

--- themes/custom/biodivPlatform/js/custom.js
+++ themes/custom/biodivPlatform/js/custom.js
@@ -23,65 +23,67 @@
       $("body").toggleClass("menu-active");
     });
 
-    // Show modal on dashboard page with use of cookies
-    var modalDashboard = new bootstrap.Modal(document.getElementById("exampleModal"));
-    /* modalDashboard.show();*/
+    if($('.page-node-16').length) {
+      // Show modal on dashboard page with use of cookies
+      var modalDashboard = new bootstrap.Modal(document.getElementById("exampleModal"));
+      /* modalDashboard.show();*/
 
-    // ajax call to check if tutorial modal has to show
-    // get id of logged in user
-    const id = $("#page-wrapper").data("user-id");
-    var hasLogged = 0;
-    $.ajax({
-      type: "GET",
-      async: false,
-      url: '/api/newlogin?_format=json&id=' + id,
-      contentType: 'application/json',
-      success: function (response) {
-        if (response == 1 || response == 0) {
-          hasLogged = response;
-        } else {
-          console.log(response);
-        }
-      },
-    });
+      // ajax call to check if tutorial modal has to show
+      // get id of logged in user
+      const id = $("#page-wrapper").data("user-id");
+      var hasLogged = 0;
+      $.ajax({
+        type: "GET",
+        async: false,
+        url: '/api/newlogin?_format=json&id=' + id,
+        contentType: 'application/json',
+        success: function (response) {
+          if (response == 1 || response == 0) {
+            hasLogged = response;
+          } else {
+            console.log(response);
+          }
+        },
+      });
 
-    $('.modal-slider').hide();
+      $('.modal-slider').hide();
 
-    if (!hasLogged) {
-      modalDashboard.show();
+      if (!hasLogged) {
+        modalDashboard.show();
+      }
+
+      $('#exampleModal .btn-close, #exampleModal .close').click(function () {
+        modalDashboard.hide();
+      });
+
+      document.getElementById("exampleModal").addEventListener('shown.bs.modal', function (event) {
+        console.log('let it slide');
+        $('.modal-slider .field--name-field-modal-slide.field__items').slick({
+          infinite: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          accessibility: true,
+          /*prevArrow: $('.modal-slider-prev'),*/
+          nextArrow: $('.modal-slider-next'),
+          dots: true
+        });
+
+        // Show close button when last slide is active
+        var slideAmount = $(".modal-slider .field--name-field-modal-slide.field__items").slick("getSlick").slideCount-1;
+
+        $('.modal-slider .field--name-field-modal-slide.field__items').on('afterChange', function(event, slick, currentSlide){
+          if(currentSlide === slideAmount ) {
+            $('.modal-slider-next').hide();
+            $('.modal-slider-final').show();
+          } else {
+            $('.modal-slider-final').hide();
+            $('.modal-slider-next').show();
+          }
+        });
+
+        $('.modal-slider').show();
+      });
     }
-
-    $('#exampleModal .btn-close, #exampleModal .close').click(function () {
-      modalDashboard.hide();
-    });
-
-    document.getElementById("exampleModal").addEventListener('shown.bs.modal', function (event) {
-      console.log('let it slide');
-      $('.modal-slider .field--name-field-modal-slide.field__items').slick({
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        accessibility: true,
-        /*prevArrow: $('.modal-slider-prev'),*/
-        nextArrow: $('.modal-slider-next'),
-        dots: true
-      });
-
-      // Show close button when last slide is active
-      var slideAmount = $(".modal-slider .field--name-field-modal-slide.field__items").slick("getSlick").slideCount-1;
-
-      $('.modal-slider .field--name-field-modal-slide.field__items').on('afterChange', function(event, slick, currentSlide){
-        if(currentSlide === slideAmount ) {
-          $('.modal-slider-next').hide();
-          $('.modal-slider-final').show();
-        } else {
-          $('.modal-slider-final').hide();
-          $('.modal-slider-next').show();
-        }
-      });
-
-      $('.modal-slider').show();
-    });
 
   });
 
