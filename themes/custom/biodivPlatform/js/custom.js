6d4ab6e9b87c071ba94b9abb5ab502b809e6c408
bartnelissen <bart@hyp3.be>
2022-07-28 12:51:45 +0200

    Dashboard pages.

--- themes/custom/biodivPlatform/js/custom.js
+++ themes/custom/biodivPlatform/js/custom.js
@@ -21,4 +21,35 @@
     $("body").toggleClass("menu-active");
   });
 
+  // Show modal on dashboard page with use of cookies
+  var modalDashboard = new bootstrap.Modal(document.getElementById("exampleModal"));
+  /* modalDashboard.show();*/
+
+  // ajax call to check if tutorial modal has to show
+  // get id of logged in user
+  const id = $("#page-wrapper").data("user-id");
+  var hasLogged = 0;
+  $.ajax({
+    type: "GET",
+    async: false,
+    url: '/api/newlogin?_format=json&id=' + id,
+    contentType: 'application/json',
+    success: function (response) {
+      if (response == 1 || response == 0) {
+        hasLogged = response;
+      } else {
+        console.log(response);
+      }
+    },
+  });
+
+  if (!hasLogged) {
+    modalDashboard.show();
+  }
+
+  $('#exampleModal .btn-close, #exampleModal .close').click(function () {
+    modalDashboard.hide();
+  });
+
+
 })(jQuery, Drupal);
