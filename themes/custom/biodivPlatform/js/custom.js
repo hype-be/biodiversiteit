12e18d5a476e909b70421cdc14710c9373ceb1a4
bartnelissen <bart@hyp3.be>
2022-07-29 15:21:31 +0200

    Theming dashboard.

--- themes/custom/biodivPlatform/js/custom.js
+++ themes/custom/biodivPlatform/js/custom.js
@@ -23,6 +23,7 @@
       $("body").toggleClass("menu-active");
     });
 
+    // Dashboard page
     if($('.page-node-16').length) {
       // Show modal on dashboard page with use of cookies
       var modalDashboard = new bootstrap.Modal(document.getElementById("exampleModal"));
@@ -57,7 +58,7 @@
       });
 
       document.getElementById("exampleModal").addEventListener('shown.bs.modal', function (event) {
-        console.log('let it slide');
+        /*console.log('let it slide');*/
         $('.modal-slider .field--name-field-modal-slide.field__items').slick({
           infinite: true,
           slidesToShow: 1,
@@ -84,6 +85,56 @@
         $('.modal-slider').show();
       });
     }
+
+    // My profile page
+    if($('.page-node-19').length) {
+
+      // Fill in all school fields for the chosen school in the modal school edit
+      $('.btn-profile-school-edit').click(function () {
+        var schoolName = $(this).attr("data-name");
+        var schoolStreet = $(this).attr("data-street");
+        var schoolPostal = $(this).attr("data-postal");
+        var schoolCity = $(this).attr("data-city");
+        $('#modal-my-profile-school-edit #school-name').val(schoolName);
+        $('#modal-my-profile-school-edit #school-street').val(schoolStreet);
+        $('#modal-my-profile-school-edit #school-postal').val(schoolPostal);
+        $('#modal-my-profile-school-edit #school-city').val(schoolCity);
+      });
+    }
+
+    // Set cookie for course module with the id of the course as it's content
+    var arrCourses = [];
+    var courseId = '';
+    if($('.node--type-course-module').length) {
+
+      courseId = $('.node--type-course-module').find('#page-wrapper').attr("date-node-id");
+      arrCourses.push(courseId);
+      console.log(arrCourses);
+
+      if (Cookies.get('coursesVisited') === undefined || Cookies.get('coursesVisited') === '') {
+        Cookies.set('coursesVisited', arrCourses);
+      }
+    }
+
+    // Trigger toasts
+    function showToast(bntEl, toastEl) {
+      const toastTrigger = document.getElementById(bntEl);
+      const toastMessage = document.getElementById(toastEl);
+      const toastTriggerText = document.getElementById(bntEl).getAttribute('data-toast-message');
+      console.log(toastTriggerText);
+      if (toastTrigger) {
+        toastTrigger.addEventListener('click', () => {
+          const toast = new bootstrap.Toast(toastMessage);
+          toast.show();
+          $(toastMessage).find('.toast-body').text(toastTriggerText);
+        })
+      }
+    }
+
+    showToast('user-save', 'toast-settings');
+    showToast('school-save', 'toast-settings');
+    showToast('school-delete', 'toast-settings');
+
 
   });
 
